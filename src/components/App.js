39f09b721ab744a1447c0325448c6bf4314c3c6c
Jacob Radcliffe <jradcli2@my.westga.edu>
2019-08-18 20:24:36 -0500

    Adds nav bar

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+    </div>
+  );
+}
+
+export default App;
